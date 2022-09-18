65382e50b769573f74b992d9d2fdbd2f06b11d9d
IljaL <ilja606060@inbox.lv>
2022-09-18 19:15:39 +0300

    last commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass        = require('gulp-sass')(require('sass'));
+const rename       = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const GulpCleanCss = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function() {
@@ -13,15 +16,22 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(rename({
+                prefix: "",
+                suffix: ".min",
+              }))
+            .pipe(autoprefixer())
+            .pipe(GulpCleanCss({compatibility: 'ie8'}))
             .pipe(gulp.dest("src/css"))
             .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel('styles'));
-    gulp.watch("*.html").on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("src/js/script.js").on("change", browserSync.reload);
 });
 
 gulp.task('default', gulp.parallel('watch','server','styles'));
--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,10 @@ function changeColor () {
       i = 0;
     }
   }
+
+  import { Application } from '@splinetool/runtime';
+
+  const canvas = document.getElementById('canvas3d');
+  const app = new Application(canvas);
+  app.load('https://prod.spline.design/y9Ktlm0mFFlnJM0D/scene.splinecode');
+  
