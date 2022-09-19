2860427ea31f45b8918707bcbca5441f6938a789
ilja <poetryspreader@yandex.com>
2022-09-20 01:45:39 +0300

    main__upd

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,19 @@
 const logo = document.querySelectorAll('.logo_text'),
-    btn = document.querySelector('.logo_box'),
-    colors = ["#FFA07A", "#3CB371", "#008B8B", '#6A5ACD', "#2F4F4F"];
+    btn = document.querySelector('.logo__box'),
+    colors = ["#FFA07A", "#3CB371", "#008B8B", "#6A5ACD", "red"];
 
-let i = 0;
+var i = 0,
+    testColor = [255,255,255];
 
-btn.addEventListener('mouseenter',changeColor);
-function changeColor () {
+
+
+function colorChange() {
+    testColor[Math.floor(Math.random() * 3) + 1]
     btn.style.backgroundColor = colors[i];
     i++;
-    if (i == colors.length){
-      i = 0;
-    }
-  }
+    if (i >= colors.length) i = 0;
+    setTimeout(colorChange, 2000);
+    console.log(colors[i])
+}
+colorChange();
 
-  import { Application } from '@splinetool/runtime';
-
-  const canvas = document.getElementById('canvas3d');
-  const app = new Application(canvas);
-  app.load('https://prod.spline.design/y9Ktlm0mFFlnJM0D/scene.splinecode');
-  
